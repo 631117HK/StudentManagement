e0ac3e84d97b02d57989f803694ed87666b0cd93
kunihiro <hk631117@icloud.com>
2025-01-24 16:16:02 +0900

    課題５完成

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -2,20 +2,42 @@ package raisetech.StudentManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Enami Kouji";
+	private String age = "37";
+	private Map<String, String> Student;
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello,World";
+	@GetMapping("/studentinfo")
+	public String getStudentinfo() {
+		return name + " " + age + "歳";
 	}
-}
+
+	//GET POST
+	//GETは取得する,リクエストの結果を受け取る
+	//POSTは情報を与える、渡す
+	@PostMapping("/name")
+	public void setStudentinfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@PostMapping("/StudentName")
+	public void updateStudentName(String name) {
+		this.name = name;
+		}
+	}
+
