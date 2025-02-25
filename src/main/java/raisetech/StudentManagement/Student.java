859f919449ac0f4ed39d1b840d98711d79209fab
kunihiro <hk631117@icloud.com>
2025-02-25 18:54:05 +0900

    課題５完成

---
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -0,0 +1,25 @@
+package raisetech.StudentManagement;
+
+public class Student {
+
+    private String name;
+    private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
